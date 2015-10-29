c0c75aa64804490d6394a191a1650522a6c288b0
olagajda <aleksandra.gajda@gmail.com>
2015-10-29 16:35:41 +0100

    created letter animations and main menu

--- js/app.js
+++ js/app.js
@@ -9,6 +9,78 @@ var Application = function() {
         mainContainer.css("height", windowHeight);
     }
 
+    function rotateLetter() {
+
+        var letter = $(".letter");
+
+        letter.on("mouseenter", function(){
+
+            $(this).toggleClass("rotate");
+        });
+
+
+    }
+
+    function colorLetter() {
+
+        var letter = $(".letter");
+        var randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+        letter.on("mouseenter", function(){
+
+            $(this).css("color", "#" + randomColor);
+
+            randomColor = Math.floor(Math.random()*16777215).toString(16);
+        });
+
+        letter.on("mouseleave", function(){
+
+            $(this).css("color", "black");
+        });
+    }
+
+    function turnLetter() {
+
+        var letter = $(".letter");
+
+        letter.on("mouseenter", function(){
+
+            $(this).toggleClass("turn");
+        });
+    }
+
+    function scaleLetter() {
+
+        var letter = $(".letter");
+
+        letter.on("mouseenter", function(){
+
+            $(this).toggleClass("scale");
+        });
+    }
+
+    function threeDLetter() {
+
+        var letter = $(".letter");
+
+        letter.one("mouseenter", function(){
+
+            $(this).toggleClass("three-d");
+        });
+    }
+
+
+    function chooseOption() {
+
+        var optionButton = $(".game");
+
+        optionButton.on("click", function(){
+
+            optionButton.removeClass("active");
+            $(this).addClass("active");
+        });
+    }
+
 
 
 
@@ -18,7 +90,13 @@ var Application = function() {
 
 
     return {
-        mainContainerHeight:mainContainerHeight
+        mainContainerHeight:mainContainerHeight,
+        rotateLetter:rotateLetter,
+        colorLetter:colorLetter,
+        turnLetter:turnLetter,
+        scaleLetter:scaleLetter,
+        threeDLetter:threeDLetter,
+        chooseOption:chooseOption
 
     };
 
@@ -32,6 +110,13 @@ $(function() {
 
     var app = new Application();
     app.mainContainerHeight();
+    //app.rotateLetter();
+    //app.colorLetter();
+    //app.turnLetter();
+    //app.scaleLetter();
+    //app.threeDLetter();
+    app.chooseOption();
+
 
 
 });
