15a6040eb9b0791bf3539b0b13120e99f1f3a7c5
olagajda <aleksandra.gajda@gmail.com>
2015-10-30 11:59:48 +0100

    finished adding content to know-me option and  added hover on letters function, improved the game option

--- js/app.js
+++ js/app.js
@@ -1,13 +1,6 @@
 var Application = function() {
 
 
-    function mainContainerHeight() {
-
-        var windowHeight = $(window).height();
-        var mainContainer = $(".main-container");
-
-        mainContainer.css("height", windowHeight);
-    }
 
     function colorLetter() {
 
@@ -27,188 +20,95 @@ var Application = function() {
         });
     }
 
-    function rotateLetter() {
 
+    function animateLetter(style, time) {
         var letter = $(".letter");
+        letter.on("mouseenter", function(){
+            $(this).addClass(style).delay(time).queue(function(){
+                $(this).removeClass(style).dequeue();
+            });
+        });
+    }
+
+
+    function knowMe(letterNo, myClass, fadeTime) {
+
+        var letter = $(".letter-" + letterNo);
+        var letter15 = $(".letter-15");
 
         letter.on("mouseenter", function(){
-
-            var oldOne = $(this);
-            var clone = oldOne.clone();
-
-            $(this).toggleClass("rotate");
-
-            setTimeout(function(){
-                $(this).removeClass("rotate");
-                //$(this).before(clone);
-                //$("." + oldOne.attr("class") + ":last").remove();
-            }, 3000);
+            $("." + myClass).fadeIn(fadeTime);
+        });
+        letter.on("mouseleave", function(){
+            $("." + myClass).fadeOut(fadeTime);
+        });
 
 
-
-
-
+        $(".letter-11").on("click", function(){
+            window.open("https://github.com/olagajda", "_blank");
+        });
+        letter15.on("mouseenter", function(){
+            $(".contact").fadeIn(500);
+        });
+        letter15.on("mouseleave", function(){
+            $(".contact").fadeOut(10000);
+        });
+        letter15.on("click", function(){
+            window.location.href = "mailto:aleksandra.gajda@gmail.com";
         });
     }
-
-
-    function turnLetter() {
-
-        var letter = $(".letter");
-
-        letter.on("mouseenter", function(){
-
-            $(this).toggleClass("turn");
-
-            setTimeout(function(){
-                $(this).removeClass("turn");
-            }, 2000);
-        });
-
-
-
-    }
-
-    function scaleLetter() {
-
-        var letter = $(".letter");
-
-        letter.on("mouseenter", function(){
-
-            $(this).toggleClass("scale");
-            setTimeout(function(){
-                $(this).removeClass("scale");
-            }, 2000);
-        });
-
-
-
-    }
-
-    function threeDLetter() {
-
-        var letter = $(".letter");
-
-        letter.on("mouseenter", function(){
-
-            $(this).toggleClass("three-d");
-
-            setTimeout(function(){
-                $(this).removeClass("three-d");
-            }, 2000);
-        });
-
-
-
-    }
-
-
-    function knowMe() {
-
-
-        $(".letter-1").on("mouseenter", function(){
-            $(".portrait").fadeIn(1000);
-        });
-        $(".letter-1").on("mouseleave", function(){
-            $(".portrait").fadeOut();
-        });
-
-        $(".letter-3").on("mouseenter", function(){
-            $(".edu").fadeIn(1000);
-        });
-        $(".letter-3").on("mouseleave", function(){
-            $(".edu").fadeOut();
-        });
-
-        $(".letter-5").on("mouseenter", function(){
-            $(".skills").fadeIn(1000);
-        });
-        $(".letter-5").on("mouseleave", function(){
-            $(".skills").fadeOut();
-        });
-
-        $(".letter-7").on("mouseenter", function(){
-            $(".experience").fadeIn(1000);
-        });
-        $(".letter-7").on("mouseleave", function(){
-            $(".experience").fadeOut();
-        });
-
-
-    }
-
-
-
-
-
-
 
     function chooseOption() {
 
         var optionButton = $(".option-button");
         var letters = $(".letter");
-        var allLettersCode = "<div class='letter letter-1'>A</div><div class='letter letter-2'>l</div><div class='letter letter-3'>e</div><div class='letter letter-4'>k</div><div class='letter letter-5'>s</div><div class='letter letter-6'>a</div><div class='letter letter-7'>n</div><div class='letter letter-8'>d</div><div class='letter letter-9'>r</div><div class='letter letter-10'>a</div> <div class='letter letter-11'>G</div><div class='letter letter-12'>a</div><div class='letter letter-13'>j</div><div class='letter letter-14'>d</div><div class='letter letter-15'>a</div>";
 
 
         optionButton.on("click", function(){
 
 
-            //letters.off();
-
-            //setTimeout(function(){
-            //    letters.remove();
-            //    $(".letters-all").html(allLettersCode);
-            //    console.log("yellow!")
-            //}, 3000);
-
-            letters.remove();
-            $(".letters-all").html(allLettersCode);
-
-
-
+            letters.off();
 
             switch($(this).data("option")) {
 
                 case "rotate":
-                    rotateLetter();
+                    animateLetter('rotate', 3000);
                     break;
                 case "color":
                     colorLetter();
+                    //animateLetter('color');
                     break;
                 case "turn":
-                    turnLetter();
+                    animateLetter('turn', 2000);
                     break;
                 case "scale":
-                    scaleLetter();
+                    animateLetter('scale', 2000);
                     break;
                 case "three-d":
-                    threeDLetter();
+                    animateLetter('three-d', 1500);
                     break;
                 case "know-me":
+                    knowMe("1", "portrait", 1000);
+                    knowMe("3", "edu", 1000);
+                    knowMe("5", "skills", 1000);
+                    knowMe("7", "experience", 1000);
+                    knowMe("11", "git", 1000);
+                    knowMe("12", "sit-on-chair", 500);
+                    knowMe("13", "bhagaskara", 500);
                     knowMe();
+
                     break;
                 default:
                     console.log("Something didn't work just right.");
             }
-
-
 
             optionButton.removeClass("active");
             $(this).addClass("active");
         });
     }
 
-
-
-
-
-
-
-
-
     return {
-        mainContainerHeight:mainContainerHeight,
-        chooseOption:chooseOption,
+        chooseOption:chooseOption
 
     };
 
@@ -218,10 +118,8 @@ var Application = function() {
 $(function() {
 
     var app = new Application();
-    app.mainContainerHeight();
     app.chooseOption();
 
 
 
 });
-
